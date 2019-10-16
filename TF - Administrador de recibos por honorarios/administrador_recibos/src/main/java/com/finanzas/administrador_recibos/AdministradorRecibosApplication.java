6c9cd9fe0cd56c66d685b29f9b8d697f85e29ba9
Cesar <u201710947@upc.edu.pe>
2019-10-16 15:17:29 -0500

    inicio de una nueva aventura

---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/AdministradorRecibosApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.administrador_recibos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradorRecibosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradorRecibosApplication.class, args);
+	}
+
+}
---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/test/java/com/finanzas/administrador_recibos/AdministradorRecibosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finanzas.administrador_recibos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdministradorRecibosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
