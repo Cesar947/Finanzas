74b3df6f151d53c1312c36292dbd721ea55e0997
Cesar <u201710947@upc.edu.pe>
2019-10-27 11:28:51 -0500

    guardar Recibo por honorarios

---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/service/ReciboHonorariosService.java
@@ -0,0 +1,8 @@
+package com.finanzas.administrador_recibos.service;
+
+import com.finanzas.administrador_recibos.model.ReciboHonorarios;
+
+public interface ReciboHonorariosService {
+
+	ReciboHonorarios registrarReciboPorHonorarios(ReciboHonorarios recibo) throws Exception;
+}
---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/service/implementation/ReciboHonorariosServiceImpl.java
@@ -0,0 +1,25 @@
+package com.finanzas.administrador_recibos.service.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.finanzas.administrador_recibos.dao.ReciboHonorariosRepository;
+import com.finanzas.administrador_recibos.model.ReciboHonorarios;
+import com.finanzas.administrador_recibos.service.ReciboHonorariosService;
+
+@Service
+public class ReciboHonorariosServiceImpl implements ReciboHonorariosService {
+
+	private ReciboHonorariosRepository reciboHonorariosRepository;
+	
+	@Autowired
+	public ReciboHonorariosServiceImpl(ReciboHonorariosRepository reciboHonorariosRepository) {
+		this.reciboHonorariosRepository = reciboHonorariosRepository;
+	}
+	
+	public ReciboHonorarios registrarReciboPorHonorarios(ReciboHonorarios recibo) throws Exception{
+	
+		return reciboHonorariosRepository.save(recibo);
+	}
+	
+}
