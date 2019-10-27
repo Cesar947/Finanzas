22b39b18e9aa971ad1c2f62eaaf819f63fda1a05
Umer-12 <umer-12@hotmail.com>
2019-10-27 12:38:00 -0500

    registrar emisor

--- TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/model/Emisor.java
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/model/Emisor.java
@@ -15,7 +15,6 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Emisor")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Emisor implements Serializable {
@@ -31,7 +30,7 @@ public class Emisor implements Serializable {
     private int dni;
 	
 	@Column(name="N_Nombres", length = 50, nullable = false)
-	private int nombres;
+	private String nombres;
 	
 	@Column(name="N_Apellidos", length = 50, nullable = false)
 	private int apellidos;
@@ -50,6 +49,79 @@ public class Emisor implements Serializable {
 	
 	@Column(name="T_Contrasena", length = 50, nullable = false)
 	private int contrasena;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public int getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(int apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(int ruc) {
+		this.ruc = ruc;
+	}
+
+	public int getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(int direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getNombre_usuario() {
+		return nombre_usuario;
+	}
+
+	public void setNombre_usuario(int nombre_usuario) {
+		this.nombre_usuario = nombre_usuario;
+	}
+
+	public int getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(int contrasena) {
+		this.contrasena = contrasena;
+	}
 	
 	
 	
--- TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/service/implementation/EmisorServiceImpl.java
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/service/implementation/EmisorServiceImpl.java
@@ -1,5 +1,28 @@
 package com.finanzas.administrador_recibos.service.implementation;
 
-public class EmisorServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finanzas.administrador_recibos.dao.EmisorRepository;
+import com.finanzas.administrador_recibos.model.Emisor;
+import com.finanzas.administrador_recibos.service.EmisorService;
+
+@Service
+public class EmisorServiceImpl implements EmisorService{
+
+	@Autowired
+	private EmisorRepository emisorRepository;
+	
+	@Override
+	@Transactional
+	public Integer insert(Emisor emisor) {
+		// TODO Auto-generated method stub
+		int rpta = emisorRepository.buscarNombreEmisor(emisor.getNombres());
+		if (rpta == 0) {
+			emisorRepository.save(emisor);
+		}
+		return rpta;
+	}
 
 }
