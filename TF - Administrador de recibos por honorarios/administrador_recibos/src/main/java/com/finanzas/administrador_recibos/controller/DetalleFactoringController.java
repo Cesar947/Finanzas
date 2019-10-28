430086d203f06b8e51950d4964da39f263907411
intel <intel@Internet>
2019-10-27 22:05:14 -0500

    Se agrego funcion registrar en factoring y detallefactoring

---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/controller/DetalleFactoringController.java
@@ -0,0 +1,27 @@
+package com.finanzas.administrador_recibos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finanzas.administrador_recibos.model.DetalleFactoring;
+import com.finanzas.administrador_recibos.service.DetalleFactoringService;
+
+@RestController
+@RequestMapping("/detallefactoring")
+public class DetalleFactoringController {
+	private DetalleFactoringService detallefactoringService;
+
+	@Autowired
+	public DetalleFactoringController(DetalleFactoringService detallefactoringService) {
+		this.detallefactoringService = detallefactoringService;
+
+	}
+
+	@RequestMapping(path = "/detallefactoring", method = RequestMethod.POST)
+	public DetalleFactoring registrarDetalleFactoring(@RequestBody DetalleFactoring detallefactoring) throws Exception {
+		return detallefactoringService.registrarDetalleFactoring(detallefactoring);
+	}
+}
---
+++ TF - Administrador de recibos por honorarios/administrador_recibos/src/main/java/com/finanzas/administrador_recibos/controller/FactoringController.java
@@ -0,0 +1,28 @@
+package com.finanzas.administrador_recibos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finanzas.administrador_recibos.model.Factoring;
+import com.finanzas.administrador_recibos.service.FactoringService;
+
+@RestController
+@RequestMapping("/factoring")
+public class FactoringController {
+	private FactoringService factoringService;
+
+	@Autowired
+	public FactoringController(FactoringService factoringService) {
+		this.factoringService = factoringService;
+
+	}
+
+	@RequestMapping(path = "/factoring", method = RequestMethod.POST)
+	public Factoring registrarFactoring(@RequestBody Factoring factoring) throws Exception {
+		return factoringService.registrarFactoring(factoring);
+	}
+
+}
